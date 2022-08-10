7f6bf09924285a5bb486a2c1045a7ec7ce1259c1
최병욱 <esc5221@gmail.com>
2022-08-10 16:00:58 +0900

    chore: default db setting (postgreSQL)

--- neis_menu/settings.py
+++ neis_menu/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'neis_menu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neis_menu',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
