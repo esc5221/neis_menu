8788ed5fbdfb53e40477c898a6ff199f5fe35f7f
최병욱 <esc5221@gmail.com>
2022-08-10 16:02:00 +0900

    feat: add School, Menu models - model 초안, 추후  field 및 meta class 변경 가능

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from schools.models import School
+
 # Create your models here.
+
+
+class Menu(models.Model):
+
+    class MenuType(models.IntegerChoices):
+        BREAKFAST = (1, '아침')
+        LUNCH = (2, '점심')
+        DINNER = (3, '저녁')
+
+    school = models.ForeignKey('schools.School', on_delete=models.CASCADE)
+    type = models.IntegerField(choices=MenuType.choices)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.school.name + ' ' + self.get_type_display() + ' ' + self.date
--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    code = models.IntegerField()
+    name = models.CharField(max_length=100, blank=False)
+    location = models.CharField(max_length=100, blank=True, null=False)
+    address = models.CharField(max_length=100, blank=True, null=False)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.name
+
