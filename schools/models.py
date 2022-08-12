65eda17e44dd299a9977d214cb7a85b711f17f8b
최병욱 <esc5221@gmail.com>
2022-08-12 18:18:11 +0900

    feat: edit School model - added and modified fields - moved model choices to separated types.py

--- schools/models.py
+++ schools/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from schools.types import SchoolTypes, EduOffices
+
 class School(models.Model):
-    code = models.IntegerField()
+
+    code = models.IntegerField(unique=True)
+    edu_office_code = models.CharField(max_length=10, choices=EduOffices.choices)
     name = models.CharField(max_length=100, blank=False)
+    type = models.IntegerField(choices=SchoolTypes.choices)
     location = models.CharField(max_length=100, blank=True, null=False)
     address = models.CharField(max_length=100, blank=True, null=False)
 
@@ -12,4 +16,3 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
-
